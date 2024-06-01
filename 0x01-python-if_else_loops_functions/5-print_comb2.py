bd042b108ff7e71b8877fae49803424347b6f37f
CSampebgo333 <clemsosampebgo7@gmail.com>
2024-06-01 13:54:04 -0700

    new file:   5-print_comb2.py

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for  number in range(100):
+    if number < 10:
+        print("0{:d}".format(number), end=", ")
+    elif number < 99:
+        print("{:d}".format(number), end=", ")
+    else:
+        print(number)
